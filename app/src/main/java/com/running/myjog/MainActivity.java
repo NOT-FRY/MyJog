8c8b26b5054bc547fabaabedfd0428803d8ae16d
NOT-FRY <francescod.lauro@gmail.com>
2022-04-20 17:45:35 +0200

    inserimento livelli con spinner

--- app/src/main/java/com/running/myjog/MainActivity.java
+++ app/src/main/java/com/running/myjog/MainActivity.java
@@ -8,17 +8,21 @@ import android.os.CountDownTimer;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private EditText corsa,camminata,ripetizioni;
     private TextView tempo,countdown;
     private int co,ca,rip,tempoTotale;
 
-
+    private Spinner spinner;
+    private static final String[] livelli = {"Tempo personalizzato","Livello 1","Livello 2","Livello 3","Livello 4","Livello 5","Livello 6","Livello 7","Livello 8","Livello 9","Livello 10"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +59,84 @@ public class MainActivity extends AppCompatActivity {
             ripetizioni.setText(""+rip);
         if(tempoTotale!=0)
             tempo.setText(tempoTotale+" minuti");
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,livelli);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                camminata.setText("");
+                corsa.setText("");
+                ripetizioni.setText("");
+                break;
+            case 1:
+                camminata.setText(""+2);
+                corsa.setText(""+1);
+                ripetizioni.setText(""+8);
+                break;
+            case 2:
+                camminata.setText(""+3);
+                corsa.setText(""+2);
+                ripetizioni.setText(""+6);
+                break;
+            case 3:
+                camminata.setText(""+3);
+                corsa.setText(""+4);
+                ripetizioni.setText(""+6);
+                break;
+            case 4:
+                camminata.setText(""+3);
+                corsa.setText(""+6);
+                ripetizioni.setText(""+5);
+                break;
+            case 5:
+                camminata.setText(""+3);
+                corsa.setText(""+10);
+                ripetizioni.setText(""+4);
+                break;
+            case 6:
+                camminata.setText(""+3);
+                corsa.setText(""+15);
+                ripetizioni.setText(""+3);
+                break;
+            case 7:
+                camminata.setText(""+5);
+                corsa.setText(""+25);
+                ripetizioni.setText(""+2);
+                break;
+            case 8:
+                camminata.setText(""+5);
+                corsa.setText(""+40);
+                ripetizioni.setText(""+1);
+                break;
+            case 9:
+                camminata.setText(""+5);
+                corsa.setText(""+45);
+                ripetizioni.setText(""+1);
+                break;
+            case 10:
+                camminata.setText(""+5);
+                corsa.setText(""+50);
+                ripetizioni.setText(""+1);
+                break;
+
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+
 
     public void start(View v){
         if(checkParameters(corsa)&&checkParameters(camminata)&&checkParameters(ripetizioni)){
